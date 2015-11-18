bc35bd316973c2e4e70faf0cc85fcb6864d81662
andy <ydnaandy123@yahoo.com.tw>
2015-11-18 16:36:27 +0800

    VideoGcp2PathPoint
    Given the raw input about the video information(label,GCP). Transorm it
    and generate the PathPoint for js to use.

---
+++ VideoGcp2PathPoint/REAMDME.md
@@ -0,0 +1,3 @@
+Transform the Video information source(../src/VideoSource).
+Combine the information about label and gcp.
+Output file is pathPoint.js under (../src/VideoSource/jsPahtPoint/).
---
+++ VideoGcp2PathPoint/VideoGcp2PathPoint.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#define DATANUM 5
+FILE *fp_label, *fp_gcp, *fp_path, *fp_pathItp;
+int pathNum=0;
+char buff[1024], diction[DATANUM*100][1024];
+bool matchError=0;
+int main(void)
+{
+    int i;
+    fp_label = fopen("../src/VideoSource/label/label_0M3Uuqw0Gn8_checked.txt","r");
+    fp_gcp = fopen("../src/VideoSource/GT_gcp/for_vsfm_GT_0M3Uuqw0Gn8.gcp","r");
+    if(fp_label==NULL || fp_gcp==NULL){
+        printf("file is NULL.");
+        return 0;
+    }
+    while(fscanf(fp_gcp,"%s",diction[DATANUM*pathNum])!=EOF){
+        printf("%s ",diction[DATANUM*pathNum]); // image_id
+        fscanf(fp_gcp,"%s",diction[DATANUM*pathNum+1]);
+        printf("%s ", diction[DATANUM*pathNum+1]); // lan
+        fscanf(fp_gcp,"%s",diction[DATANUM*pathNum+2]);
+        printf("%s ", diction[DATANUM*pathNum+2]); //lon
+        fscanf(fp_gcp,"%s",diction[DATANUM*pathNum+3]);
+        printf("%s ", diction[DATANUM*pathNum+3]); // 0?
+        if(fscanf(fp_label,"%s",diction[DATANUM*pathNum+4])==EOF){
+            matchError=1; break;
+        }
+        printf("%s\n",diction[DATANUM*pathNum+4]); // pano_id
+        fscanf(fp_label,"%s",buff); //image_id
+        pathNum++;
+    }
+    fclose(fp_label);
+    fclose(fp_gcp);
+    if(pathNum>0){
+        fp_path = fopen("../src/VideoSource/jsPahtPoint/pathPoint.js","w");
+        fprintf(fp_path,"pathPoint={");
+        fprintf(fp_path,"%s,%s",diction[1],diction[2]);
+        for(i=1;i<pathNum;i++){
+            fprintf(fp_path,",%s,%s",diction[DATANUM*i+1],diction[DATANUM*i+2]);
+        }
+        fprintf(fp_path,"};");
+    }
+    fclose(fp_path);
+    return 0;
+}
